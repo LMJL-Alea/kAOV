429ec9075bf3300cd6f58fbd96dfd5d9c16d24fb
parsenteva <polina.arsenteva@univ-nantes.fr>
2026-07-29 17:43:27 +0200

    Configure datasets module

---
+++ src/kaov/datasets/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 17:40:32 2026
+
+@author: Polina Arsenteva
+"""
+
+from kaov.datasets._base import (
+    load_reversion,
+    load_rabbits_anndata,
+)
+
