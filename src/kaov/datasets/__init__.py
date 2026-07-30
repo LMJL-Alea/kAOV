91cb1158e1e1b10d700e9ae0d01482a277f0bce9
parsenteva <polina.arsenteva@univ-nantes.fr>
2026-07-30 14:20:19 +0200

    Modify init files

--- src/kaov/datasets/__init__.py
+++ src/kaov/datasets/__init__.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 29 17:40:32 2026
-
-@author: Polina Arsenteva
-"""
-
 from kaov.datasets._base import (
     load_reversion,
     load_rabbits_anndata,
--- src/kaov/datasets/data/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 30 11:00:35 2026
-
-@author: Polina Arsenteva
-"""
-
